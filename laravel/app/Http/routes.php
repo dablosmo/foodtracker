bea953ebb3a85e37ae0450cdcdb3cbc1e2b9c3a8
David Zhang <David@Davids-MacBook-Pro.local>
2015-04-28 00:01:45 -0700

    User account creation / validation added based on lecture code

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -1,21 +1,72 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\User;
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('signup', function()
+{
+	return view('signup');
+});
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::post('signup', function()
+{
+  $validation = User::validate(Request::all());
+
+  if ($validation->passes()) {
+    $user = new User();
+    $user->name = Request::input('name');
+    $user->email = Request::input('email');
+    $user->password = Hash::make(Request::input('password'));
+    $user->save();
+
+    //    return redirect('login');
+    Auth::loginUsingId($user->id);
+    return redirect('dashboard');
+  }
+
+  return redirect('signup')->withInput()->withErrors($validation->errors());
+});
+
+Route::get('login', function()
+{
+  return view('login');
+});
+
+Route::post('login', function()
+{
+  $remember_me = Request::input('remember_me') === 'on' ? true : false;
+
+  if (Auth::attempt(['email' => Request::input('email'), 'password' => Request::input('password')], $remember_me)) {
+//    return redirect('dashboard');
+    return redirect()->intended();
+  }
+
+  return redirect('login');
+});
+
+Route::get('dashboard', function()
+{
+  if (Auth::check()) {
+    return view('dashboard');
+  }
+
+  return redirect('login');
+});
+
+Route::get('dashboard', ['middleware' => 'auth', function()
+{
+  return view('dashboard');
+}]);
+
+Route::get('logout', function()
+{
+  Auth::logout();
+  return redirect('login');
+});
+
+
+Route::get('password', ['middleware' => 'auth', function()
+{
+  dd('edit password page');
+}]);
